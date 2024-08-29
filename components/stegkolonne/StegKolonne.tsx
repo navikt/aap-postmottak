27f674a654ad50159383602617aaa09a9cd2a2f8
Halvor Grizzly Bjørn <halvor.grizzly.bjorn@nav.no>
2024-08-29 15:51:33 +0200

    :sparkles: Det er lurt å gjenbruke kode for vilkårskort og asynkron datafetching
    Co-authored-by: Tor Idland <tor.idland@nav.no>

--- components/stegkolonne/StegKolonne.tsx
+++ components/stegkolonne/StegKolonne.tsx
@@ -1,4 +1,10 @@
+import { VilkårsKort } from 'components/vilkårskort/VilkårsKort';
+
 export const StegKolonne = ({ aktivtSteg }: { aktivtSteg: string }) => {
   // Det er her vi gjør datafetching og rendering av stegene
-  return <div>{aktivtSteg}</div>;
+  return (
+    <div>
+      <VilkårsKort heading={aktivtSteg}>Litt innhold i vilkårskortet</VilkårsKort>
+    </div>
+  );
 };
