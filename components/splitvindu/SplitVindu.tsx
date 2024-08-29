8b3f87520da66154224199cb9d242b5453f650b2
Tor Idland <tor.idland@nav.no>
2024-08-29 10:37:40 +0200

    :children_crossing: Åpne/lukke split view

--- components/splitvindu/SplitVindu.tsx
+++ components/splitvindu/SplitVindu.tsx
@@ -1,9 +1,10 @@
 'use client'
 
-import {HGrid} from "@navikt/ds-react";
+import {Button, HGrid} from "@navikt/ds-react";
 import {ReactNode, useState} from "react";
 import {Dokumentvisning} from "components/dokumentvisning/Dokumentvisning";
 import styles from './SplitVindu.module.css'
+import {ExpandIcon, ShrinkIcon} from "@navikt/aksel-icons";
 
 interface Props {
     children: ReactNode;
@@ -11,11 +12,14 @@ interface Props {
 export const SplitVindu = ({children}: Props) => {
     const [isSplit, setIsSplit] = useState<boolean>(true);
     return (
-       <HGrid columns={isSplit ? 2 : 1} rows={'1fr'} className={styles.splitVindu} >
-           <div>
-               {children}
-           </div>
-           {isSplit && <Dokumentvisning />}
-       </HGrid>
+        <HGrid columns={isSplit ? 2 : 1} rows={'1fr'} className={styles.splitVindu} >
+            <div>
+                <div className={styles.splitStateButtonWrapper}>
+                    <Button icon={isSplit ? <ExpandIcon /> : <ShrinkIcon />} type={'button'} onClick={() => setIsSplit(!isSplit)} />
+                </div>
+                {children}
+            </div>
+            {isSplit && <Dokumentvisning />}
+        </HGrid>
     )
 }
