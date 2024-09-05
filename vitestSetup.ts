0154c0d5730fd0f153d72f495560b437f0be71ad
Tor Idland <tor.idland@nav.no>
2024-09-05 09:31:54 +0200

    :white_check_mark: mock next/navigation og bytt til config.mts

--- vitestSetup.ts
+++ vitestSetup.ts
@@ -1,6 +1,15 @@
-import { afterEach } from 'vitest';
+import {afterEach, beforeAll, vi} from 'vitest';
 import '@testing-library/jest-dom/vitest';
 import { cleanup } from '@testing-library/react';
+
+beforeAll(() => {
+  vi.mock('next/navigation', () => ({
+    useParams: vi
+        .fn()
+        .mockReturnValue({id: '123', aktivtSteg: 'HEIHEI'}),
+    useRouter: vi.fn()
+  }))
+})
 
 afterEach(() => {
   cleanup();
