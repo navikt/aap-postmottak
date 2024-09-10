cc873f328a5b80b72cbdfded1355df49e9ed7395
Tor Idland <tor.idland@nav.no>
2024-09-10 15:29:10 +0200

    :green_check_mark: test at combobox vises i kategoriseringssteget

--- components/kategoriser/Kategoriser.test.tsx
+++ components/kategoriser/Kategoriser.test.tsx
@@ -2,6 +2,7 @@ import { describe, it, expect } from 'vitest';
 import { render, screen } from '@testing-library/react';
 import { Kategoriser } from './Kategoriser';
 import { AvklarTemaGrunnlag, KategoriserGrunnlag } from 'lib/types/types';
+import {FinnSak} from "../finnsak/FinnSak";
 
 describe('Kategoriser', () => {
   const grunnlag: KategoriserGrunnlag = {
@@ -12,5 +13,9 @@ describe('Kategoriser', () => {
     render(<Kategoriser behandlingsVersjon={1} journalpostId={'123'} grunnlag={grunnlag} />);
     const heading = screen.getByText('Kategoriser');
     expect(heading).toBeVisible();
+  });
+  it('Har et valg for å knytte dokumentet til sak', () => {
+    render(<Kategoriser behandlingsVersjon={1} journalpostId={'123'} grunnlag={grunnlag} />);
+    expect(screen.getByRole('combobox', { name: 'Kategoriser' })).toBeVisible();
   });
 });
