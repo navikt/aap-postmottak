9fe39f9ceccab95fc080054007015b5d76185b75
Tor Idland <tor.idland@nav.no>
2025-01-14 12:43:04 +0100

    :lock: disable toolbar i pdfvisning

--- components/dokumentvisning/Dokumentvisning.tsx
+++ components/dokumentvisning/Dokumentvisning.tsx
@@ -46,7 +46,7 @@ export const Dokumentvisning = ({ journalpostId, dokumenter }: Props) => {
         ))}
       </HStack>
       {dataUri && (
-        <object data={dataUri} type="application/pdf" width="100%" height="100%">
+        <object data={`${dataUri}#toolbar=0`} type="application/pdf" width="100%" height="100%">
           <p>
             Alternative text - include a link <a href="http://africau.edu/images/default/sample.pdf">to the PDF!</a>
           </p>
